f51ff3997a980a6d6c385b6ff67266e3558c742f
Dionisia Montanez <dmontanez15@gmail.com>
2018-08-14 14:30:40 -0700

    completed wave one

---
+++ lib/adagrams.rb
@@ -0,0 +1,13 @@
+def draw_letters()
+  letters = [ "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z" ]
+  # puts letters
+end
+draw_letters
+
+ten_letters = []
+1.times do |letter|
+  letter = draw_letters.shuffle.pop(10)
+  ten_letters << letter
+end
+
+puts ten_letters
