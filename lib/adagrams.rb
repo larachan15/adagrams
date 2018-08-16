6cd1df6ab0a2f26a4ffd1009742f6ea5f0397af2
Layla <layla.bristol@gmail.com>
2018-08-15 21:20:14 -0700

    updated wave 2

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -4,19 +4,16 @@ def stores_letters
   letters = [ "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z" ]
 end
 
-def shuffle_letters
+def draw_letters
   letter = stores_letters.shuffle.pop(10)
-  return letter.join(' , ')
+  return letter
 end
-
 
 def welcome
   puts "Welcome to adagrams. Here are your letters."
   shuffle_letters
 end
 
-
-puts welcome
 
 def game_instructions
   puts "Based on these letters, give us a word."
@@ -25,23 +22,22 @@ end
 
 def get_user_input
   user_input = gets.chomp
+  return user_input
 end
 
 # wave 2
-def uses_available_letters(get_user_input, shuffle_letters)
-  user_input = get_user_input.upcase.split("")
-  if user_input.include?(shuffle_letters) == true
-  else
-    puts "You entered in a word that contains characters not in the letter bank"
-    game_instructions
-    get_user_input
+def uses_available_letters?(input, letters_in_hand)
+  input = input.upcase.split("")
+  input.each do |letter|
+    if letters_in_hand.include?(letter)
+      letters_in_hand.delete(letter)
+    else
+      return false
+    end
   end
+  return true
 end
 
-puts uses_available_letters(user_input, shuffle_letters)
-
-# binding.pry
-# letters_in_hand = draw_letters
-# input = user_input.slice(0..9)
-# test = letters_in_hand.include?(user_input)
-# return test
+# wave 3
+def score_word(word)
+end
